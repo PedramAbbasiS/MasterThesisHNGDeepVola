1201301878422cb0d44d4268507e52dc2fd24602
henrikbrautmeier <38861599+henrikbrautmeier@users.noreply.github.com>
2019-08-20 10:38:19 +0200

    update

--- Python_Code/MonteCarlo.py
+++ Python_Code/MonteCarlo.py
@@ -12,38 +12,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import hngoption as hng
-from help_fun import *  
-"""    
-dt = 1
-num_path = 10000
-sz_alpha = [0.001,0.002]
-sz_gamma = [0.002,0.03]
-sz_beta = [0.001,0.02]
-sz_lambda = [-0.0005,0.00003]
-sz_omega =[0.0001,0.0002]
+from help_fun import HNG_MC  
+
+#model parameters 
+# Szenario Analyse
+#=============================================================================
+sz_alpha = [0.01,0.02]
+sz_gamma = [0.2,0.3]
+sz_beta = [0.2,0.5]
+sz_lambda = [-0.5,1.3]
+sz_omega =[0.1,0.2]
 sz_maturity = [10,20]
 sz_S0  = [1,1.2]
-sz_rate = [0,0.01]
-sz_moneyness =  np.arange(0.9,1.15,0.05)
+sz_rate = [0,0.05/252]
 param_option_dict ={}
-#model parameters 
-"""   
-# =============================================================================
-#     
-# for alpha in sz_alpha:
-#         for beta in sz_beta:
-#             for gamma in sz_gamma:
-#                 for lam in sz_lambda:
-#                     for omega in sz_omega:
-#                         sigma2 =  omega/(1-alpha*gamma**2-beta) #unconditional variance as garch initialisation
-#                         #underlyings
-#                         for T in sz_maturity:#eventuell unnötig langer pfad modellieren und kürzen?!
-#                             for S_0 in sz_S0:#eventuell unnötig (S/K nur relevant? in realen daten? dunno)
-#                                 for rate in sz_rate:
-#                                     param_option_dict[(alpha,beta,gamma,omega,lam,T,S_0,rate,"c")] =  np.mean(np.maximum(S_T[:,np.newaxis] - K,np.zeros((S_T.shape[0],K.shape[0]))),axis=0)
-#                                     param_option_dict[(alpha,beta,gamma,omega,lam,T,S_0,rate,"p")] =  np.mean(np.maximum(K-S_T[:,np.newaxis],np.zeros((S_T.shape[0],K.shape[0]))),axis=0)
-#                      
-# =============================================================================
+Moneyness = np.array([0.85,0.9,0.95,1,1.05,1.1,1.15])
+dt =1
+d_lambda_star = -0.5   
+for alpha in sz_alpha:
+    for beta in sz_beta:
+        for gamma in sz_gamma:       
+            for d_lambda in sz_lambda:
+                gamma_star = gamma+d_lambda+0.5                       
+                for omega in sz_omega:                         
+                    sigma2 =  omega/(1-alpha*gamma**2-beta) #unconditional variance as garch initialisation
+                    for T in sz_maturity: #eventuell unnötig langer pfad modellieren und kürzen?!                            
+                        for S_0 in sz_S0:
+                            K = Moneyness*S_0
+                            for rate in sz_rate:        
+                                p_call,p_put = HNG_MC(alpha,beta,gamma,omega,d_lambda,S_0,K,rate,T,dt,2)
+                                param_option_dict[(alpha,beta,gamma,omega,d_lambda,T,S_0,rate,"c")] =  p_call
+                                param_option_dict[(alpha,beta,gamma,omega,d_lambda,T,S_0,rate,"p")] =  p_put
+                      
+#=============================================================================
 
 dt = 1                                          #Zeitschritt                        
 alpha = 0.01    
@@ -56,13 +57,13 @@ omega = 0.1                                     #intercept
 PutCall = 1
 S=30
 r=0.03/252
-K=17
+K=np.array([17,40])
 T = 20
-sigma2 = omega/(1-alpha*gamma**2-beta)          #unconditional variance VGARCH
+sigma2 = (omega+alpha)/(1-alpha*gamma**2-beta)          #unconditional variance VGARCH
 V = sigma2
 
 price_rn= hng.HNC(alpha, beta, gamma_star, omega, d_lambda_star, V, S, K, r, T, PutCall) #V= inital sigma2
-price_MC_rn = HNG_MC(alpha,beta,gamma,omega,d_lambda,S,K,r,T,dt,PutCall)
+price_MC_rn = HNG_MC(alpha,beta,gamma,omega,d_lambda,S,K,r,T,dt,PutCall+1)
 print(price_rn,price_MC_rn)
 print((price_rn-price_MC_rn)/price_rn*100)
 #price_p = hng.HNC(alpha, beta, gamma, omega, d_lambda, V, S, K, r, T, PutCall)
--- Python_Code/help_fun.py
+++ Python_Code/help_fun.py
@@ -4,16 +4,48 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import hngoption as hng
 
-def HNG_MC(alpha,beta,gamma,omega,lam,S_0,K,rate,T,dt,PutCall,num_path = int(1e6), 
-           risk_neutral = True):
-    sigma2 =  omega/(1-alpha*gamma**2-beta)
+def HNG_MC(alpha, beta, gamma, omega, lam, S_0, K, rate, T, dt, PutCall = 2, num_path = int(1e6), 
+           risk_neutral = True, Variance_specs = "unconditional"):
+    """
+    This function calculates the Heston-Nandi-GARCH(1,1) option price of european calls/puts with MonteCarloSim
+    Requirements: numpy
+    Model Parameters (riskfree adjust will be done automatically): 
+        alpha,beta,gamma,omega,lam
+    Underlying:
+        S_0 starting value, K np.array of Strikes, dt Timeshift, T maturity in dt, r riskfree rate in dt
+    Function Parameters:
+        Putcall: option type (1=call,0=put,2=both)
+        num_path: number of sim paths
+        risk_neutral: Baysian, type of simulation
+        Variance_specs: Type of inital variance or inital variance input
+    
+    (C) Henrik Brautmeier, Lukas Wuertenberger 2019
+    """
+    
+    gamma_star = gamma+lam+0.5
+    
+    # Variance Input =========================================================
+    if Variance_specs=="uncondtional":
+        V = (omega+alpha)/(1-alpha*gamma**2-beta)
+    elif Variance_specs=="uncondtional forecast":
+        sigma2=(omega+alpha)/(1-alpha*gamma_star**2-beta)
+        V = omega+alpha-2*gamma_star*np.sqrt(sigma2)+(beta+gamma_star**2)*sigma2
+    elif any(type(Variance_specs)==s for s in [float,int,type(np.array([0])[0]),type(np.array([0.0])[0])]):
+        V = Variance_specs  #checks if Variance_specs is a float,int,numpy.int32 or numpy.float64
+    else:
+        print("Variance format not recognized. Uncondtional Variance will be used")
+        V = (omega+alpha)/(1-alpha*gamma**2-beta) 
+    # ========================================================================
+    
+    # Initialisation
     r = np.exp(rate*dt)-1                             
     lnS = np.zeros((num_path,T+1))
     h = np.zeros((num_path,T+1))
     lnS[:,0] = np.log(S_0)*np.ones((num_path))
-    h[:,0] = sigma2*np.ones((num_path)) #initial wert
+    h[:,0] = V*np.ones((num_path)) #initial wert
     z = np.random.normal(size=(num_path,T+1))
-    gamma_star = gamma+lam+0.5
+    
+    # Simulation
     for t in np.arange(dt,T+dt,dt):
         if risk_neutral:
             h[:,t] = omega+beta*h[:,t-dt]+alpha*(z[:,t-dt]-gamma_star*np.sqrt(h[:,t-dt]))**2
@@ -22,7 +54,11 @@ def HNG_MC(alpha,beta,gamma,omega,lam,S_0,K,rate,T,dt,PutCall,num_path = int(1e6
             h[:,t] = omega+beta*h[:,t-dt]+alpha*(z[:,t-dt]-gamma*np.sqrt(h[:,t-dt]))**2
             lnS[:,t] = lnS[:,t-dt]+r+lam*h[:,t]+np.sqrt(h[:,t])*z[:,t]
     S_T = np.exp(lnS[:,-1])
-    if PutCall:
-        return np.exp(-rate*T)*np.mean(np.maximum(S_T-K,0))
-    else:
-        return np.exp(-rate*T)*np.mean(np.maximum(K-S_T,0))  
+    
+    # Output
+    if PutCall==1: # Call
+        return np.exp(-rate*T)*np.mean(np.maximum(S_T[:,np.newaxis] - K,np.zeros((S_T.shape[0],K.shape[0]))),axis=0)
+    elif PutCall==0: # Put
+        return np.exp(-rate*T)*np.mean(np.maximum(K-S_T[:,np.newaxis],np.zeros((S_T.shape[0],K.shape[0]))),axis=0)
+    elif PutCall==2: # (Call,Put)
+        return np.exp(-rate*T)*np.mean(np.maximum(S_T[:,np.newaxis] - K,np.zeros((S_T.shape[0],K.shape[0]))),axis=0),np.exp(-rate*T)*np.mean(np.maximum(K-S_T[:,np.newaxis],np.zeros((S_T.shape[0],K.shape[0]))),axis=0)
