0ef234a38e58cc83da80e33a3b2fd979b94bc8d9
Lukas Wuertenberger <lukas.wuertenberger@uni-konstanz.de>
2019-08-23 12:58:30 +0200

    up

--- Python_Code/MonteCarlo_L.py
+++ Python_Code/MonteCarlo_L.py
@@ -14,66 +14,94 @@ from scipy.optimize import minimize
 import hngoption as hng
 from help_fun import HNG_MC,HNG_MC_simul
 from tempfile import TemporaryFile
+import py_vollib.black_scholes.implied_volatility as vol
 
-
-#model parameters 
-# Szenario Analyse
 #=============================================================================
 szenario_data =[]
-szenario_data_closed = []
 dt =1
-Maturity = np.array([20, 30, 80, 180, 250])
+Maturity = np.array([20, 50, 80, 110, 140])
 K = np.array([0.9, 0.95, 0.975, 1, 1.025, 1.05, 1.1])
 S = 1
 r = 0
 j = 0
 l = 0
 d_lambda = 0
-vola_closed = np.zeros((len(Maturity),len(K)))
-#
-Nsim = int(1e3)
-#
+Nsim = int(4.5e4)         #number of samples
+Nmaturities = len(Maturity)
+Nstrikes = len(K)
 for i in range(Nsim):
-    vola = np.zeros((2,2))
+    price = np.zeros((2,2))
+    ###########################################################################
+    #only to watch computational status
     j = j+1
     if (j == 1000):
         l = l + 1
         print(l*1000)
         j = 0 
-    while (vola < 1e-4).any():
+    ###########################################################################    
+    while ((price < 1e-9).any()) or ((price > .5).any()):  #to avoid numerical problems and too extreme scenarios
         alpha = 1
         beta = 1
         gamma_star = 1
-        omega = 1
-        while (beta+alpha*gamma_star**2 > 1) and ((omega + alpha) / (1-alpha*gamma_star**2-beta) < 1e-4):
-            alpha = np.random.uniform(low=1e-4, high=1e-2)
-            beta = np.random.uniform(low=.3, high=1)  
-            gamma_star = np.random.uniform(low=20, high=60)
-            omega = np.random.uniform(low=1e-3, high=1e-6)  
-        vola = HNG_MC_simul(alpha, beta, gamma_star, omega, d_lambda, S, K, r, Maturity, dt, output=1)
-    #V = (omega+alpha)/(1-alpha*gamma_star**2-beta) 
-#    for m in range(len(Maturity)):
-#        for k in range(len(K)):
-#            vola_closed[m,k] = hng.HNC(alpha, beta, gamma_star, omega, -.5, V, S, K[k], r, Maturity[m], PutCall=1)
-#    MC_diff = np.mean(np.abs((vola - vola_closed)/vola_closed))       
-    szenario_data.append(np.concatenate((np.asarray([alpha,beta,gamma_star,omega, 
-                                                     beta+alpha*gamma_star**2]).reshape((1,5)), vola.reshape((1,vola.shape[0]*vola.shape[1]))),axis=1))   
+        while (beta+alpha*gamma_star**2 > 1):           #stationary condition
+            alpha = np.random.uniform(low=5e-7, high=5e-5)
+            beta = np.random.uniform(low=.1, high=.6)  
+            gamma_star = np.random.uniform(low=300, high=500)
+        omega = np.random.uniform(low=1e-6, high=9e-6) 
+        d_lambda = np.random.uniform(low=.4, high=2.4) 
+        price = HNG_MC_simul(alpha, beta, gamma_star, omega, d_lambda, S, K,
+                             r, Maturity, dt, output=1, risk_neutral = False)  
+    szenario_data.append(np.concatenate((np.asarray([alpha,beta,gamma_star,omega,d_lambda, 
+                                                     beta+alpha*gamma_star**2]).reshape((1,6)), price.reshape((1,price.shape[0]*price.shape[1]))),axis=1))   
                                
-#szenario_data_1 = np.asarray(szenario_data).reshape((int(4e4),40)) 
-szenario_data_2 = np.asarray(szenario_data).reshape((Nsim,40))
-print(np.count_nonzero(szenario_data_2))
-print(np.min(szenario_data_2[:,5:]))
+szenario_data_2 = np.asarray(szenario_data).reshape((Nsim,6+Nstrikes*Nmaturities))
+print("number of nonzeros price-data: ", np.count_nonzero(szenario_data_2))
+print("max price: ", np.max(szenario_data_2[:,6:]))
+print("min price: ", np.min(szenario_data_2[:,6:]))
+print("mean price: ", np.mean(szenario_data_2[:,6:]))
+print("median price: ", np.median(szenario_data_2[:,6:]))
 
+flag = 'c'
+y_p=szenario_data_2[:,6:]
+iv = np.full_like(y_p, 0.0)
+stri = np.zeros((Nmaturities, Nstrikes))
+for i in range(Nsim):
+    price_new = y_p[i,:].reshape((Nmaturities, Nstrikes))
+    for m in range(Nmaturities):
+        for k in range(Nstrikes):
+            try:
+                stri[m,k] = vol.implied_volatility(price_new[m,k], S, K[k], Maturity[m]/252, r, flag)
+            except:
+                pass
+    iv[i] =  stri.reshape((1,len(Maturity)*len(K)))  
+
+print("number of nonteros vola-data: ", np.count_nonzero(iv))
+print("max price: ", np.max(iv))
+print("min price: ", np.min(iv))
+print("mean vola: ", np.mean(iv))
+print("mean vola: ", np.median(iv))
+
+data_connect = np.concatenate((szenario_data_2[:,:6], iv), axis=1)
+np.save('data_IV_4e4_MC_1e4', data_connect)
 #mean_diff = np.mean(szenario_data_2[:,5])
 #max_diff = np.max(szenario_data_2[:,5])
-#np.save('data', szenario_data_1)
+#np.save('data_shortmat_MC_1e5', szenario_data_2)
 
 
-                
+  
+
+
+
+# old code
+#==============================================================================
+#==============================================================================               
 #szenario_vola_calls[(alpha,beta,gamma_star,omega)] =  vola.reshape((1,vola.shape[0]*vola.shape[1]))
                 
 # #===========================================================================
-
+#    for m in range(len(Maturity)):
+#        for k in range(len(K)):
+#            vola_closed[m,k] = hng.HNC(alpha, beta, gamma_star, omega, -.5, V, S, K[k], r, Maturity[m], PutCall=1)
+#    MC_diff = np.mean(np.abs((vola - vola_closed)/vola_closed))  
                
                
 #  
--- Python_Code/NN1.py
+++ Python_Code/NN1.py
@@ -10,21 +10,40 @@ from keras.layers import Dense
 from keras import backend as K
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+import py_vollib.black_scholes.implied_volatility as vol
+#S = 1
+#flag = 'c'
+#r = .0
 #to do: load data
-#data=np.load('rBergomiTrainSet.txt')
+#datatest =np.load('rBergomiTrainSet.txt')
+#y_testdata=data[:,Nparameters:]
+#print(np.mean(y_testdata))
+#print(np.min(y_testdata))
+#print(np.max(y_testdata))
 #strikes=np.array([0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5 ])
 #maturities=np.array([0.1,0.3,0.6,0.9,1.2,1.5,1.8,2.0 ])
-data = np.load('data.npy')
-data = data[(data[:,4:]>1e-4).all(axis=1)]
+#data = np.load('data_shortmat_MC_1e5.npy')
+data = np.load('data_IV_1e4_MC_1e4.npy')
 #data1 = data[(data!=0).all(axis=1)]
 #np.count_nonzero(data1)
-Nparameters = 4
-maturities = np.array([20, 30, 80, 180, 250])
+Nparameters = 5
+maturities = np.array([20, 50, 80, 110, 140])
 strikes = np.array([0.9, 0.95, 0.975, 1, 1.025, 1.05, 1.1])
 Nstrikes = len(strikes)   
 Nmaturities = len(maturities)   
 xx=data[:,:Nparameters]
 yy=data[:,Nparameters+1:]
+
+#yy = np.full_like(y_p, 0.0)
+#stri = np.zeros((len(maturities), len(strikes)))
+#for i in range(len(y_p)):
+#    price_new = y_p[i,:].reshape((len(maturities), len(strikes)))
+#    for m in range(len(maturities)):
+#        for k in range(len(strikes)):
+#            stri[m,k] = vol.implied_volatility(price_new[m,k], S, strikes[k], maturities[m]/252, r, flag)
+#    yy[i] =  stri.reshape((1,len(maturities)*len(strikes)))   
+
+
 #yy=data[:,Nparameters:]
 
 #Nstrikes = 20         #????
@@ -54,7 +73,8 @@ def ytransform(y_train,y_val,y_test):
    
 def yinversetransform(y):
     return scale.inverse_transform(y)
-[y_train, y_val, y_test]=ytransform(y_train, y_val, y_test)
+
+[y_train_trafo, y_val_trafo, y_test_trafo]=ytransform(y_train, y_val, y_test)
 
 ub=np.amax(xx, axis=0)
 lb=np.amin(xx, axis=0)
@@ -71,9 +91,9 @@ def myinverse(x):
         
     return res
 
-X_train = np.array([myscale(x) for x in X_train])
-X_val = np.array([myscale(x) for x in X_val])
-X_test = np.array([myscale(x) for x in X_test])
+X_train_trafo = np.array([myscale(x) for x in X_train])
+X_val_trafo = np.array([myscale(x) for x in X_val])
+X_test_trafo = np.array([myscale(x) for x in X_test])
 
 #Neural Network
 keras.backend.set_floatx('float64')
@@ -89,13 +109,24 @@ def root_mean_squared_error(y_true, y_pred):
         return K.sqrt(K.mean(K.square(y_pred - y_true)))
         
 NN1.compile(loss = root_mean_squared_error, optimizer = "adam")
-NN1.fit(X_train, y_train, batch_size=32, validation_data = (X_val, y_val),
+NN1.fit(X_train_trafo, y_train_trafo, batch_size=32, validation_data = (X_val_trafo, y_val_trafo),
         epochs = 200, verbose = True, shuffle=1)
 
 
+test = yinversetransform(NN1.predict(X_test_trafo))
+y_test_trans = yinversetransform(y_test_trafo)
+error = np.abs((test - y_test_trans))/np.abs(y_test_trans)
+error1 = np.zeros((len(error[0,:])))
+for i in range(len(error[0,:])):
+    error1[i] = np.mean(error[:,i])
+    
+error2 = np.zeros((len(t[0,:])))
+for i in range(len(t[0,:])):
+    error2[i] = np.mean(t[:,i])
+
 S0=1.
-y_test_re = yinversetransform(y_test)
-prediction=[yinversetransform(NN1.predict(X_test[i].reshape(1,Nparameters))[0]) for i in range(len(X_test))]
+y_test_re = yinversetransform(y_test_trafo)
+prediction=[yinversetransform(NN1.predict(X_test_trafo[i].reshape(1,Nparameters))[0]) for i in range(len(X_test_trafo))]
 plt.figure(1,figsize=(14,4))
 ax=plt.subplot(1,3,1)
 err = np.mean(100*np.abs((y_test_re-prediction)/y_test_re),axis = 0)
