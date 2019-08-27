4cf422d18eb34af75adb2aeab6abd603033ecc90
Lukas Wuertenberger <lukas.wuertenberger@uni-konstanz.de>
2019-08-27 11:05:18 +0200

    up

--- Data/PreparationFunctions/Filter.m
+++ Data/PreparationFunctions/Filter.m
@@ -1,9 +1,10 @@
 clearvars
 load('/Users/Lukas/Documents/GitHub/SeminarOptions/Data/Datasets/SP500/SP500_date_prices_returns.mat');
-%SP500_dates = datetime(SP500_date_prices_returns(1,:),'ConvertFrom','datenum');
-%SP500_dates.Format = 'dd.MM.yyyy';
+SP500_dates1 = datetime(SP500_date_prices_returns(1,:),'ConvertFrom','datenum');
+SP500_dates1.Format = 'dd.MM.yyyy';
 %SP500_year = year(SP500_dates);
 SP500_dates = SP500_date_prices_returns(1,:);
+SP500_year = year(SP500_dates1);
 
 SP500_prices(1,:) = SP500_date_prices_returns(2,:);
 SP500_logreturns(1,:) = zeros(size(SP500_date_prices_returns(2,:)));
@@ -12,6 +13,7 @@ SP500_logreturns(1,2:end) = log(SP500_date_prices_returns(2,2:end)./...
 
 SP500 = struct;
 SP500.date = SP500_dates';
+SP500.year = SP500_year';
 SP500.price = SP500_prices';
 SP500.logreturn = SP500_logreturns';
 writetable(struct2table(SP500), 'SP500_data.txt')
