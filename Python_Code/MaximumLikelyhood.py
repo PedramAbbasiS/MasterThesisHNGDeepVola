b0f4d148d993c58970967c5505de22a246351c9e
henrikbrautmeier <38861599+henrikbrautmeier@users.noreply.github.com>
2019-09-02 09:07:43 +0200

    fetch

--- Python_Code/MaximumLikelyhood.py
+++ Python_Code/MaximumLikelyhood.py
@@ -6,47 +6,34 @@ Theta = (omega,alpha,beta,gamma,lambda)
 import numpy as np
 from scipy.optimize import minimize
 from datetime import date
-from scipy import stats
-import pyopt
+from help_fun import ll_hng_n
 dates =[]
 data = np.loadtxt("C:/Users/henri/Documents/SeminarOptions/Python_Code/SP500_data/SP500_data.txt",delimiter=',')
 for i in range(data.shape[0]):
     data[i,0]-=366 # conversion formula matlab to python
     dates.append(date.fromordinal(int(data[i,0])))
 
-idx = data[:,1]==2012
-logret = data[idx,3]
-# Loglikelyhood with normal error
 
-#initialised with uncond. variance
-def ll_hng_n(par0,x,r=0,history=False):
-    # "par0" is a vector containing the parameters over# which the optimization will be performed
-    # "x" is a vector containing the historical log returns on the underlying asset
-    # "r" is the risk-free rate, expressed here on a daily basis
-    omega=par0[0]   # the variance’s intercept
-    alpha=par0[1]   # the autoregressive parameter
-    beta=par0[2]    # the persistence parameter
-    gamma=par0[3]   # the leverage parameter
-    lambda0=par0[4] # the risk premium
-    loglik=0        # the log-likelihood is initialized at 0
-    h=(omega+alpha)/(1-beta-alpha*gamma**2) # initianlize variance as longterm variance
-    h2 = np.zeros((len(x)+1,1))
-    h2[0] = h
-    for i in range(0,len(x)):
-        # The conditional log-likelihood at time i is:
-        #temp = -0.5*np.log(2*np.pi)-0.5*np.log(h)-0.5*(x[i]-r-lambda0*h)**2/h
-        temp = stats.norm.logpdf(x[i],r+lambda0*h,np.sqrt(h))
-        loglik=loglik+temp
-        # An the conditional variance is updated as well
-        h=omega+alpha*(x[i]-r-(lambda0+gamma)*h)**2/h+beta*h
-        h2[i+1] = h
-    if history:
-        return -loglik,h2
-    else:
-        return -loglik
-             
-         
+    
 # Opti Params:
+#params = np.zeros((4,5))
+l = 0
+par0 = np.array([1.8e-06,1.3e-6,0.67,448,13])
+
+#for i in [2012,2013,2014,2015]:
+idx = data[:,1]==2012#i
+logret = data[idx,3]
+#result = minimize(ll_hng_n, par0, method='Nelder-Mead',args = (logret),options={'disp': True,'ftol': 1e-6})
+hng_stat = [{'type': 'ineq','fun': lambda x: 1 - (x[3]**2)*x[1]-x[2]-1e-8},{'type': 'ineq','fun':lambda x: x[2]},{'type': 'ineq','fun':lambda x:x[1]},{'type': 'ineq','fun':lambda x:x[0]-1e-8}]
+#boundaries = [(0,100,True),(0,100,True),(0,100,True),(-10000,10000,True),(-10000,10000,True)]
+#boundaries =[]
+#result = minimize(ll_hng_n, par0, method='COBYLA',args = (logret), constraints = hng_stat, options={'disp': True,'tol':1e-5,'rhobeg': (1e-6,1e-6,0.1,2,0.5),'catol':(0,0,0),'maxiter':1000})
+result = minimize(ll_hng_n, par0, method='SLSQP',args = (logret),constraints=hng_stat,options={'disp': True,'ftol':1e-8})
+print(result.x)
+#params[l,:]=result.x
+if ((result.x[3]**2)*result.x[1]-result.x[2])<1:
+    par0 = result.x
+l += 1
 
 par = np.array([6e-07,1.49e-6,0.66,460,0.64])         
          
