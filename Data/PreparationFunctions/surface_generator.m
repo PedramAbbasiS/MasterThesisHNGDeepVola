5e25350ff89f3a8a87b0e945db8a33a2d4c4db3a
henrikbrautmeier <38861599+henrikbrautmeier@users.noreply.github.com>
2019-09-04 13:39:51 +0200

    data generator

--- Data/PreparationFunctions/surface_generator.m
+++ Data/PreparationFunctions/surface_generator.m
@@ -1,4 +1,4 @@
-function surface = surface_generator(year,stock_ind,Strikes,Maturity,path,bound,plot,vecormat,saver)
+function surface = surface_generator(year,stock_ind,Strikes,Maturity,path,bound,plot,vecormat,saver,volaprice)
 % Function smooths a volatility surface of data points to a given grid 
 % Input: 
 %   year:       int             -   year of data 
@@ -9,14 +9,23 @@ function surface = surface_generator(year,stock_ind,Strikes,Maturity,path,bound,
 %   plot:       logical         -   show surface plot
 %   vecormat:   logical         -   output as vector or matrix
 %   saver:      logical         -   save out as .mat-file
+%   volaprice:  logical         -   volatility or prices    
+%
 % Output:
 %   surface data
 %   optional: .mat-file,surfaceplot
 % (c) Henrik Brautmeier, Lukas Wuertenberger 2019
 
 path_ = strcat(path,'/',stock_ind,'/','Calls',num2str(year),'.mat');
-load(path_,'TradingDaysToMaturity','ImpliedVolatilityoftheOption','Moneyness','Volume','OpenInterestfortheOption');
-data = [TradingDaysToMaturity,1./Moneyness,ImpliedVolatilityoftheOption,Volume,OpenInterestfortheOption];
+if volaprice
+    load(path_,'TradingDaysToMaturity','ImpliedVolatilityoftheOption','Moneyness','Volume','OpenInterestfortheOption');
+    data = [TradingDaysToMaturity,1./Moneyness,ImpliedVolatilityoftheOption,Volume,OpenInterestfortheOption];
+    specs = 'vola';
+else
+    load(path_,'TradingDaysToMaturity','MeanOptionPrice','Moneyness','Volume','OpenInterestfortheOption','TheSP500PriceThisDate');
+    data = [TradingDaysToMaturity,1./Moneyness,MeanOptionPrice./TheSP500PriceThisDate,Volume,OpenInterestfortheOption];
+    specs = 'price';
+end   
 data = data(data(:,5)>=bound(1) &data(:,4)>=bound(1),1:3);
 x = data(:,1);
 y = data(:,2);
@@ -27,20 +36,21 @@ surface_data = griddata(x,y,z,X,Y);
 if vecormat
     surface = reshape(surface_data,[1,length(Maturity)*length(Strikes)]);
     if saver
-        name = strcat('surface',num2str(year),stock_ind);
+        name = strcat('surface',specs,num2str(year),stock_ind);
         save(name,'surface')
     end
 else 
     surface = surface_data;
     if saver
-        name = strcat('surface',num2str(year),stock_ind);
+        name = strcat('surface',specs,num2str(year),stock_ind);
         save(name,'surface')
     end
 end
 if plot 
     figure('Name','RealData')
     scatter3(x,y,z,'filled')
-    title(strcat('Scatterplot: Calls of',stock_ind,'in ',num2str(year)))
-    figure
+    title(strcat('Scatterplot: Call',specs,' of ',stock_ind,' in ',num2str(year)))
+    figure('Name','Fitted Data')
     surf(X,Y,surface_data)
+    title(strcat('Scatterplot: Call',specs,' of ',stock_ind,' in ',num2str(year)))
 end
