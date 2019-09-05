68f665538bac2402c3f4a8258e67b65a442a1810
Lukas Wuertenberger <lukas.wuertenberger@uni-konstanz.de>
2019-09-05 14:53:59 +0200

    up

--- Code/Blanka Code/Example_rBergomi.py
+++ Code/Blanka Code/Example_rBergomi.py
@@ -23,6 +23,7 @@ import time
 import scipy
 
 data =np.load('rBergomiTrainSet.txt')
+np.savetxt('rBergomiTrainSet_new', data, delimiter=',')
 strikes=np.array([0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5])
 maturities=np.array([0.1,0.3,0.6,0.9,1.2,1.5,1.8,2.0 ])
 Nparameters = 4
