b3aedb82d924db1eea227354071a194e78ca3cf9
henrikbrautmeier <38861599+henrikbrautmeier@users.noreply.github.com>
2019-09-06 11:15:40 +0200

    vola

--- Data/PreparationFunctions/RealDataTrafo.m
+++ Data/PreparationFunctions/RealDataTrafo.m
@@ -6,7 +6,18 @@ stock_ind = 'SP500';
 Strikes = 0.9:0.02:1.1;
 Maturity = 30:30:240;
 bound =[100,100];
-for year = 2010:2015
+for years = 2010:2015
     surface = surface_generator(year,stock_ind,Strikes,Maturity,path,bound,1,1,1,0,'linear');
 end
+%
+dates =SP500_date_prices_returns(1,:); 
 log_returns= log(1+SP500_date_prices_returns(3,:));
+dates =year(datetime(dates,'ConvertFrom','datenum'));
+i = 1;
+vola = zeros(6,1);
+for years = 2010:2015
+    logr = log_returns(dates==years);
+    vola(i) = sqrt(252*var(logr));
+    i=i+1;
+end
+save('vola','vola')
