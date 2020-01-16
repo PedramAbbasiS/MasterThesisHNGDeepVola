5ba10c181b26f54d88317d6d863020c634ecea73
henrikbrautmeier <38861599+henrikbrautmeier@users.noreply.github.com>
2020-01-16 22:11:38 +0100

    convexity constraint theoretically implemented

--- Code/NN1_henrik_cnn2.py
+++ Code/NN1_henrik_cnn2.py
@@ -13,6 +13,7 @@ import matplotlib.ticker as mtick
 import time
 import scipy
 import scipy.io
+import tensorflow as tf
 
 # Data Import
 
@@ -74,30 +75,6 @@ X_test_trafo  = X_test_trafo.reshape((Ntest,5,1,1))
 X_train_trafo = X_train_trafo.reshape((Ntrain,5,1,1))
 X_val_trafo   = X_val_trafo.reshape((Nval,5,1,1))
 
-#Neural Network
-keras.backend.set_floatx('float64')
-NN1 = Sequential() 
-NN1.add(InputLayer(input_shape=(Nparameters,1,1,)))
-NN1.add(ZeroPadding2D(padding=(2, 2)))
-NN1.add(Conv2D(32, (3, 1), padding='valid',strides =(1,1),activation='elu'))#X_train_trafo.shape[1:],activation='elu'))
-NN1.add(ZeroPadding2D(padding=(1,1)))
-NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(1,1),activation ='elu'))
-NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(2,1),activation ='elu'))
-NN1.add(ZeroPadding2D(padding=(1,1)))
-NN1.add(Conv2D(32, (3,3),padding='valid',strides =(2,1),activation ='elu'))
-NN1.add(ZeroPadding2D(padding=(1,1)))
-
-NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(2,1),activation ='elu'))
-NN1.add(ZeroPadding2D(padding=(1,1)))
-
-NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(2,1),activation ='elu'))
-#NN1.add(MaxPooling2D(pool_size=(2, 1)))
-#NN1.add(Dropout(0.25))
-#NN1.add(ZeroPadding2D(padding=(0,1)))
-NN1.add(Conv2D(Nstrikes, (2, 1),padding='valid',strides =(2,1),activation ='linear', kernel_constraint = keras.constraints.NonNeg()))
-#NN1.add(MaxPooling2D(pool_size=(4, 1)))
-NN1.summary()
-
 def root_mean_squared_error(y_true, y_pred):
         return K.sqrt(K.mean(K.square(y_pred - y_true)))
     
@@ -105,24 +82,82 @@ def root_relative_mean_squared_error(y_true, y_pred):
         return K.sqrt(K.mean(K.square((y_pred - y_true)/y_true)))    
 def root_relative_mean_squared_error_convexity(param):
     def help_error(y_true, y_pred):
+        error = K.sqrt(K.mean(K.square((y_pred - y_true)/y_true)))
         for i in range(1,Nmaturities-1):
             for j in range(1,Nstrikes-1):
-                convexity = K.mean(K.control_flow_ops.math_ops.logical_or(K.greater(0.5*(y_pred[:,0,i-1,j]+y_pred[:,0,i+1,j]),y_pred[:,0,i,j]),K.greater(0.5*(y_pred[:,0,i,j-1]+y_pred[:,0,i,j+1]),y_pred[:,0,i,j])))       
-        return K.sqrt(K.mean(K.square((y_pred - y_true)/y_true)))+convexity*param
+                convexity = K.mean(K.control_flow_ops.math_ops.logical_or(K.greater(y_pred[:,0,i,j],0.5*(y_pred[:,0,i-1,j]+y_pred[:,0,i+1,j])),K.greater(y_pred[:,0,i,j],0.5*(y_pred[:,0,i,j-1]+y_pred[:,0,i,j+1]))))       
+                error = error+param*convexity/((Nstrikes-1)*(Nmaturities-1))
+        return error
     return help_error
-                
-NN1.compile(loss = root_relative_mean_squared_error_convexity(param=0.001), optimizer = "adam",metrics=["MAPE","MSE"])
-#NN1.compile(loss = "mean_squared_error", optimizer = "adam",metrics=["MAPE"])
+#Neural Network
+keras.backend.set_floatx('float64')
+saver = np.zeros(np.arange(0,0.6,0.05).shape)
+count = 0
+for param in np.arange(0,0.6,0.05):
+    NN1 = Sequential() 
+    NN1.add(InputLayer(input_shape=(Nparameters,1,1,)))
+    NN1.add(ZeroPadding2D(padding=(2, 2)))
+    NN1.add(Conv2D(32, (3, 1), padding='valid',strides =(1,1),activation='elu'))#X_train_trafo.shape[1:],activation='elu'))
+    NN1.add(ZeroPadding2D(padding=(1,1)))
+    NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(1,1),activation ='elu'))
+    NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(2,1),activation ='elu'))
+    NN1.add(ZeroPadding2D(padding=(1,1)))
+    NN1.add(Conv2D(32, (3,3),padding='valid',strides =(2,1),activation ='elu'))
+    NN1.add(ZeroPadding2D(padding=(1,1)))
+    
+    NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(2,1),activation ='elu'))
+    NN1.add(ZeroPadding2D(padding=(1,1)))
+    
+    NN1.add(Conv2D(32, (2, 2),padding='valid',strides =(2,1),activation ='elu'))
+    #NN1.add(MaxPooling2D(pool_size=(2, 1)))
+    #NN1.add(Dropout(0.25))
+    #NN1.add(ZeroPadding2D(padding=(0,1)))
+    NN1.add(Conv2D(Nstrikes, (2, 1),padding='valid',strides =(2,1),activation ='linear', kernel_constraint = keras.constraints.NonNeg()))
+    #NN1.add(MaxPooling2D(pool_size=(4, 1)))
+    #NN1.summary()
+    
+    
+                    
+    NN1.compile(loss = root_relative_mean_squared_error_convexity(param), optimizer = "adam",metrics=["MAPE","MSE"])
+    #NN1.compile(loss = "mean_squared_error", optimizer = "adam",metrics=["MAPE"])
+    #NN1.compile(loss = root_relative_mean_squared_error_lasso, optimizer = "adam",metrics=[root_relative_mean_squared_error,"mean_squared_error"])
+    #NN1.compile(loss = 'mean_absolute_percentage_error', optimizer = "adam")
+    NN1.fit(X_train_trafo, y_train_trafo, batch_size=64, validation_data = (X_val_trafo, y_val_trafo),
+            epochs = 30, verbose = True, shuffle=1)
+    #NN1.save_weights('NN_HNGarch_weights.h5')
+    saver[count] = NN1.evaluate(X_val_trafo, y_val_trafo)[1]
+    print(count)
+    count += 1 
 #NN1.compile(loss = root_relative_mean_squared_error_lasso, optimizer = "adam",metrics=[root_relative_mean_squared_error,"mean_squared_error"])
 #NN1.compile(loss = 'mean_absolute_percentage_error', optimizer = "adam")
-NN1.fit(X_train_trafo, y_train_trafo, batch_size=64, validation_data = (X_val_trafo, y_val_trafo),
-        epochs = 50, verbose = True, shuffle=1)
-#NN1.save_weights('NN_HNGarch_weights.h5')
 
-#NN1.compile(loss = root_relative_mean_squared_error_lasso, optimizer = "adam",metrics=[root_relative_mean_squared_error,"mean_squared_error"])
-#NN1.compile(loss = 'mean_absolute_percentage_error', optimizer = "adam")
+plt.figure(figsize=(14,4))
+plt.scatter(np.arange(0,0.6,0.05),saver)
+plt.xlabel("constraint parameter",fontsize=15,labelpad=5)
+plt.ylabel("validation error",fontsize=15,labelpad=5)
+plt.show()
+count = 0
+error = np.zeros(np.arange(0,0.6,0.05).shape)
+def convex_violation(y_pred):
+    error = np.zeros(y_pred.shape[0])
+    for i in range(Nmaturities):
+        if (i==0 or i==Nmaturities-1):
+            for j in range(1,Nstrikes-1):
+                convexity = y_pred[:,0,i,j]>0.5*(y_pred[:,0,i,j-1]+y_pred[:,0,i,j+1])       
+                error +=   convexity
 
+        else: 
+            for j in range(1,Nstrikes-1):
+                convexity = np.logical_or(y_pred[:,0,i,j]>0.5*(y_pred[:,0,i-1,j]+y_pred[:,0,i+1,j]),y_pred[:,0,i,j]>0.5*(y_pred[:,0,i,j-1]+y_pred[:,0,i,j+1]))       
+                error +=   convexity
 
+    for j in [0,Nstrikes-1]:
+        for i in range(1,Nmaturities-1):
+            convexity = y_pred[:,0,i,j]>0.5*(y_pred[:,0,i-1,j]+y_pred[:,0,i+1,j])     
+            error +=   convexity
+    mean_error = np.mean(error)
+    return  error,mean_error    
+       
 
 #==============================================================================
 #error plots
@@ -213,9 +248,9 @@ plt.ylabel("Maturity",fontsize=15,labelpad=5)
 
 plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
 plt.tight_layout()
-
-plt.savefig('HNG_NNErrors_Cnn2.png', dpi=300)
 plt.show()
+plt.savefig('HNG_NNErrors_Cnn2.png', dpi=300)
+
 
 
 
---
+++ Code/utlility function/additional_functions.py
@@ -0,0 +1,57 @@
+""" List of different usefull functions:
+    custom errors, datatransformations, etc
+"""
+
+# Sclaes Parameter to be between -1 and 1
+def myscale(x):
+    res=np.zeros(Nparameters)
+    for i in range(Nparameters):
+        res[i]=(2*x[i] - (ub[i] + lb[i])) / (ub[i] - lb[i])
+    return res
+def myinverse(x):
+    res=np.zeros(Nparameters)
+    for i in range(Nparameters):
+        res[i]=x[i]*(ub[i] - lb[i]) *0.5 + (ub[i] + lb[i])*0.5
+    return res
+
+# calculates number of constraint violations for parameter combinations
+def constraint_violation(x):
+    return np.sum(x[:,0]*x[:,2]**2+x[:,1]>=1)/x.shape[0],x[:,0]*x[:,2]**2+x[:,1]>=1,x[:,0]*x[:,2]**2+x[:,1]  
+
+
+
+def root_mean_squared_error(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true)))   
+def root_relative_mean_squared_error(y_true, y_pred):
+        return K.sqrt(K.mean(K.square((y_pred - y_true)/y_true)))    
+
+def rmse_constraint(param):
+    def rel_mse_constraint(y_true, y_pred):
+            traf_a = 0.5*(y_pred[:,0]*diff[0]+bound_sum[0])
+            traf_g = 0.5*(y_pred[:,2]*diff[2]+bound_sum[2])
+            traf_b = 0.5*(y_pred[:,1]*diff[1]+bound_sum[1])
+            constraint = traf_a*K.square(traf_g)+traf_b
+            #constraint = K.variable(value=constraint, dtype='float64')
+            return K.sqrt(K.mean(K.square((y_pred - y_true)/y_true)))  +param*K.mean(K.greater(constraint,1))
+    return rel_mse_constraint
+
+def mse_constraint(param):
+    def rel_mse_constraint(y_true, y_pred):
+            traf_a = 0.5*(y_pred[:,0]*diff[0]+bound_sum[0])
+            traf_g = 0.5*(y_pred[:,2]*diff[2]+bound_sum[2])
+            traf_b = 0.5*(y_pred[:,1]*diff[1]+bound_sum[1])
+            constraint = traf_a*K.square(traf_g)+traf_b
+            #constraint = K.variable(value=constraint, dtype='float64')
+            return tf.keras.losses.MSE( y_true, y_pred) +param*K.mean(K.greater(constraint,1))
+    return rel_mse_constraint
+
+
+def root_relative_mean_squared_error_convexity(param):
+    def help_error(y_true, y_pred):
+        error = K.sqrt(K.mean(K.square((y_pred - y_true)/y_true)))
+        for i in range(1,Nmaturities-1):
+            for j in range(1,Nstrikes-1):
+                convexity = K.mean(K.control_flow_ops.math_ops.logical_or(K.greater(y_pred[:,0,i,j],0.5*(y_pred[:,0,i-1,j]+y_pred[:,0,i+1,j])),K.greater(y_pred[:,0,i,j],0.5*(y_pred[:,0,i,j-1]+y_pred[:,0,i,j+1]))))       
+                error = error+param*convexity/((Nstrikes-1)*(Nmaturities-1))
+        return error
+    return help_error
